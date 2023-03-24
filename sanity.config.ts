ca98ad633d79f32db2a73f57a8d6585e4c11d0a2
528982tts <mmuzammiloff@gmail.com>
2023-03-24 15:57:20 +0500

    updated deploy sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,6 +3,7 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
+import { vercelDeployTool } from 'sanity-plugin-vercel-deploy'
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
 
@@ -15,8 +16,7 @@ export default defineConfig({
 projectId : "qg8dvno7",
   dataset: "production",
 
-  plugins: [deskTool(), visionTool()],
-
+  plugins: [deskTool(), visionTool(),vercelDeployTool(),],
   schema: {
     types: schemaTypes,
   },
