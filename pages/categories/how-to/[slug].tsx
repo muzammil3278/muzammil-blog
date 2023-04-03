d304440896066b96e4672a844d90ab6dd74efa05
528982tts <mmuzammiloff@gmail.com>
2023-04-03 13:09:03 +0500

    updated

--- pages/categories/how-to/[slug].tsx
+++ pages/categories/how-to/[slug].tsx
@@ -1,191 +1,200 @@
-// import { SanityDocument } from "@sanity/client";
-// import { GetStaticPaths, GetStaticProps } from "next";
-// import { groq } from "next-sanity";
-// import { client } from "../../../libs/sanity.clients";
-// import Movie from "../../../components/Movie";
+// // import { SanityDocument } from "@sanity/client";
+// // import { GetStaticPaths, GetStaticProps } from "next";
+// // import { groq } from "next-sanity";
+// // import { client } from "../../../libs/sanity.clients";
+// // import Movie from "../../../components/Movie";
 
+// // const query = groq`*[_type == "posts" && slug.current == $slug][0]{
+// //   title,
+// //   poster,
+// //   overview
+// // }`;
+
+// // // Prepare Next.js to know which routes already exist
+// // export const getStaticPaths: GetStaticPaths = async () => {
+// //   const paths = await client.fetch(
+// //     groq`*[_type == "posts" && defined(slug.current)][]{
+// //       "params": { "slug": slug.current }
+// //     }`
+// //   );
+
+// //   return { paths, fallback: true };
+// // };
+
+// // export const getStaticProps: GetStaticProps = async ({ params }) => {
+// //   const queryParams = { slug: params?.slug ?? `` };
+
+// //   const movie = await client.fetch(query, queryParams);
+
+// //   return {
+// //     props: {
+// //       data: { movie },
+// //     },
+// //   };
+// // };
+
+// // export default function Page({ data }: { data: { movie: SanityDocument } }) {
+// //   return <Movie movie={data.movie} />
+// // }
+
+// import {SanityDocument} from '@sanity/client'
+// import {GetStaticPaths, GetStaticProps} from 'next'
+// import {groq} from 'next-sanity'
+// import {client} from '../../../libs/sanity.clients'
+// import Left from '@/components/blog/left'
+// import {PortableText} from '@portabletext/react'
+// import Head from 'next/head'
+// import Link from 'next/link'
+// import Poster from '@/components/blog/write/image'
+// import Heading from '@/components/blog/write/head'
+// import Author from '@/components/blog/write/author'
+// import Banner from '@/components/category/coding/banner'
+// import imageUrlBuilder from "@sanity/image-url";
+// const builder = imageUrlBuilder(client);
 // const query = groq`*[_type == "posts" && slug.current == $slug][0]{
-//   title,
+//   _id,
+//   title, 
+//   body,
 //   poster,
-//   overview
-// }`;
+//   overview,
+//   publishedAt,
+//   "slug":slug.current,
+//   author->{name,bio,poster,slug},  
+//   category->{title,"slug": slug.current},  
+//   "tag": tag[]->{title,"slug": slug.current},
+  
+// }`
 
-// // Prepare Next.js to know which routes already exist
 // export const getStaticPaths: GetStaticPaths = async () => {
 //   const paths = await client.fetch(
 //     groq`*[_type == "posts" && defined(slug.current)][]{
 //       "params": { "slug": slug.current }
 //     }`
-//   );
+//   )
 
-//   return { paths, fallback: true };
-// };
+//   return {paths, fallback: false}
+// }
 
-// export const getStaticProps: GetStaticProps = async ({ params }) => {
-//   const queryParams = { slug: params?.slug ?? `` };
+// export const getStaticProps: GetStaticProps = async ({params}) => {
+//   const queryParams = {slug: params?.slug ?? ``}
 
-//   const movie = await client.fetch(query, queryParams);
+//   const post = await client.fetch(query, queryParams)
 
 //   return {
 //     props: {
-//       data: { movie },
+//       post,
 //     },
-//   };
-// };
+//   }
+// }
+// type TagList = {
+//   title: string
+//   slug: string
+// }
+// type PageProps = {
+//   post: {
+//     tag: Array<TagList>
+//     body: []
+//     publishedAt: string
+//     author: any
+//     category: any
+//     overview: string
+//     title: string
+//     poster: string
+//     slug: string
+//     movie: SanityDocument
+//   }
+// }
+// const DOMAIN = 'https://muzammil-blog.vercel.app/'
+// const canonical = DOMAIN
 
-// export default function Page({ data }: { data: { movie: SanityDocument } }) {
-//   return <Movie movie={data.movie} />
+// export default function Page({post}: PageProps) {
+//   const {title = 'title', category, author, publishedAt, tag, poster, body = []} = post
+//   return (
+//     <>
+//      {/* content=*/}
+//       <Head>
+//         <title>{post.title}</title>
+//         <meta name="description" content={post.overview} />
+//         <meta key="og_title" property="og:title" content={post.title} />
+//         <meta key="og_description" property="og:description" content={post.overview} />
+//         <meta key="og_url" property="og:url" 
+//         // content={canonical ?? DOMAIN}  
+//         // {`${title} | ${siteName}`} 
+//         content={post.slug} 
+//         />
+//         <meta key="og_image" property="og:image" 
+//         // content={post.poster} 
+//          content={builder.image(post.poster).url()}
+//         />
+//         <meta key="og_image:alt" property="og:image:alt" content={post.title} />
+
+//         <meta name="robots" content="index,follow" />
+//         <meta key="twitter:title" property="twitter:title" content={post.title} />
+//         <meta key="twitter:description" property="twitter:description" content={post.overview} />
+//         <link rel="canonical" href={canonical ?? DOMAIN} />
+
+//       </Head>
+//       <div className="grid lg:grid-cols-3 grid-cols-1 gap-4 md:p-4 p-1">
+//         <div className="col-span-2">
+//           <main className="bg-white md:p-3 p-1">
+//             <Poster poster={poster} title={title} />
+//             <div className="context pl-2">
+//               <Heading
+//                 date={publishedAt}
+//                 author={author.name}
+//                 authorlink={author.slug.current}
+//                 cat={category.title}
+//                 catlink={category.slug}
+//                 title={title}
+//               />
+//             </div>
+
+//             <div className="p-4  leading-10 tracking-wide">
+//               <p>
+//                 <PortableText value={body} />
+//               </p>
+//             </div>
+
+//             <div className="tag">
+//               <ul className="flex">
+//                 {tag && (
+//                   <>
+//                     {tag.map((tags: any) => (
+//                       <li key={tags}>
+//                         <Link href={tags.slug}>{tags.title}</Link>
+//                       </li>
+//                     ))}
+//                   </>
+//                 )}
+//               </ul>
+//             </div>
+//             {/* <Banner /> */}
+//             <Author
+//               author={author.name}
+//               poster={author.poster}
+//               bio={author.bio}
+//               date={publishedAt}
+//             />
+//           </main>
+//         </div>
+//         <div>
+//           <Left />
+//         </div>
+//       </div>
+//     </>
+//   )
 // }
 
-import {SanityDocument} from '@sanity/client'
-import {GetStaticPaths, GetStaticProps} from 'next'
-import {groq} from 'next-sanity'
-import {client} from '../../../libs/sanity.clients'
-import Left from '@/components/blog/left'
-import {PortableText} from '@portabletext/react'
-import Head from 'next/head'
-import Link from 'next/link'
-import Poster from '@/components/blog/write/image'
-import Heading from '@/components/blog/write/head'
-import Author from '@/components/blog/write/author'
-import Banner from '@/components/category/coding/banner'
-import imageUrlBuilder from "@sanity/image-url";
-const builder = imageUrlBuilder(client);
-const query = groq`*[_type == "posts" && slug.current == $slug][0]{
-  _id,
-  title, 
-  body,
-  poster,
-  overview,
-  publishedAt,
-  "slug":slug.current,
-  author->{name,bio,poster,slug},  
-  category->{title,"slug": slug.current},  
-  "tag": tag[]->{title,"slug": slug.current},
-  
-}`
+// interface data {
+//   title: string
+//   slug: string
+// }
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = await client.fetch(
-    groq`*[_type == "posts" && defined(slug.current)][]{
-      "params": { "slug": slug.current }
-    }`
-  )
 
-  return {paths, fallback: false}
-}
+import React from 'react'
 
-export const getStaticProps: GetStaticProps = async ({params}) => {
-  const queryParams = {slug: params?.slug ?? ``}
-
-  const post = await client.fetch(query, queryParams)
-
-  return {
-    props: {
-      post,
-    },
-  }
-}
-type TagList = {
-  title: string
-  slug: string
-}
-type PageProps = {
-  post: {
-    tag: Array<TagList>
-    body: []
-    publishedAt: string
-    author: any
-    category: any
-    overview: string
-    title: string
-    poster: string
-    slug: string
-    movie: SanityDocument
-  }
-}
-const DOMAIN = 'https://muzammil-blog.vercel.app/'
-const canonical = DOMAIN
-
-export default function Page({post}: PageProps) {
-  const {title = 'title', category, author, publishedAt, tag, poster, body = []} = post
+export default function index() {
   return (
-    <>
-     {/* content=*/}
-      <Head>
-        <title>{post.title}</title>
-        <meta name="description" content={post.overview} />
-        <meta key="og_title" property="og:title" content={post.title} />
-        <meta key="og_description" property="og:description" content={post.overview} />
-        <meta key="og_url" property="og:url" 
-        // content={canonical ?? DOMAIN}  
-        // {`${title} | ${siteName}`} 
-        content={post.slug} 
-        />
-        <meta key="og_image" property="og:image" 
-        // content={post.poster} 
-         content={builder.image(post.poster).url()}
-        />
-        <meta key="og_image:alt" property="og:image:alt" content={post.title} />
-
-        <meta name="robots" content="index,follow" />
-        <meta key="twitter:title" property="twitter:title" content={post.title} />
-        <meta key="twitter:description" property="twitter:description" content={post.overview} />
-        <link rel="canonical" href={canonical ?? DOMAIN} />
-
-      </Head>
-      <div className="grid lg:grid-cols-3 grid-cols-1 gap-4 md:p-4 p-1">
-        <div className="col-span-2">
-          <main className="bg-white md:p-3 p-1">
-            <Poster poster={poster} title={title} />
-            <div className="context pl-2">
-              <Heading
-                date={publishedAt}
-                author={author.name}
-                authorlink={author.slug.current}
-                cat={category.title}
-                catlink={category.slug}
-                title={title}
-              />
-            </div>
-
-            <div className="p-4  leading-10 tracking-wide">
-              <p>
-                <PortableText value={body} />
-              </p>
-            </div>
-
-            <div className="tag">
-              <ul className="flex">
-                {tag && (
-                  <>
-                    {tag.map((tags: any) => (
-                      <li key={tags}>
-                        <Link href={tags.slug}>{tags.title}</Link>
-                      </li>
-                    ))}
-                  </>
-                )}
-              </ul>
-            </div>
-            {/* <Banner /> */}
-            <Author
-              author={author.name}
-              poster={author.poster}
-              bio={author.bio}
-              date={publishedAt}
-            />
-          </main>
-        </div>
-        <div>
-          <Left />
-        </div>
-      </div>
-    </>
+    <div>[slug]</div>
   )
-}
-
-interface data {
-  title: string
-  slug: string
 }
